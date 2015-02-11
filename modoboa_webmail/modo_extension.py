423177373320fce02ee688685fbbf4723191fff1
Antoine Nguyen <tonio@ngyn.org>
2015-02-11 10:33:34 +0100

    Initial commit.

---
+++ modoboa_webmail/modo_extension.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+"""Declare and register the webmail extension."""
+
+from django.utils.translation import ugettext_lazy
+
+from modoboa.core.extensions import ModoExtension, exts_pool
+from modoboa.lib import parameters
+
+
+class Webmail(ModoExtension):
+    name = "modoboa_webmail"
+    label = "Webmail"
+    version = "1.0"
+    description = ugettext_lazy("Simple IMAP webmail")
+    needs_media = True
+    available_for_topredirection = True
+    url = "webmail"
+
+    def load(self):
+        from .app_settings import ParametersForm, UserSettings
+
+        parameters.register(ParametersForm, "Webmail")
+        parameters.register(UserSettings, "Webmail")
+        from . import general_callbacks
+
+exts_pool.register_extension(Webmail)
---
+++ setup.py
@@ -0,0 +1,78 @@
+"""Setup script for modoboa-admin."""
+
+import re
+import os
+from setuptools import setup, find_packages
+
+ROOT = os.path.dirname(__file__)
+
+
+PIP_REQUIRES = os.path.join(ROOT, "requirements.txt")
+
+
+def parse_requirements(*filenames):
+    """
+    We generate our install_requires from the pip-requires and test-requires
+    files so that we don't have to maintain the dependency definitions in
+    two places.
+    """
+    requirements = []
+    for f in filenames:
+        for line in open(f, 'r').read().split('\n'):
+            # Comment lines. Skip.
+            if re.match(r'(\s*#)|(\s*$)', line):
+                continue
+            # Editable matches. Put the egg name into our reqs list.
+            if re.match(r'\s*-e\s+', line):
+                pkg = re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line)
+                requirements.append("%s" % pkg)
+            # File-based installs not supported/needed. Skip.
+            elif re.match(r'\s*-f\s+', line):
+                pass
+            else:
+                requirements.append(line)
+    return requirements
+
+
+def parse_dependency_links(*filenames):
+    """
+    We generate our dependency_links from the pip-requires and test-requires
+    files for the dependencies pulled from github (prepended with -e).
+    """
+    dependency_links = []
+    for f in filenames:
+        for line in open(f, 'r').read().split('\n'):
+            if re.match(r'\s*-[ef]\s+', line):
+                line = re.sub(r'\s*-[ef]\s+', '', line)
+                line = re.sub(r'\s*git\+https', 'http', line)
+                line = re.sub(r'\.git#', '/tarball/master#', line)
+                dependency_links.append(line)
+    return dependency_links
+
+
+def read(fname):
+    """A simple function to read the content of a file."""
+    return open(os.path.join(ROOT, fname)).read()
+
+
+setup(
+    name="modoboa-webmail",
+    version='1.0.0',
+    url='http://modoboa.org/',
+    license='MIT',
+    description="The webmail of Modoboa",
+    long_description=read('README.rst'),
+    author='Antoine Nguyen',
+    author_email='tonio@ngyn.org',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=parse_requirements(PIP_REQUIRES),
+    dependency_links=parse_dependency_links(PIP_REQUIRES),
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Framework :: Django',
+                 'Intended Audience :: System Administrators',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Internet :: WWW/HTTP']
+)

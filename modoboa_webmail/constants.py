bf3aaa81664703f3d3b7a2bc1d073ac4871d8d40
Antoine Nguyen <tonio@ngyn.org>
2018-01-01 11:59:54 +0100

    Display message size and allow sorting.
    see #95

--- modoboa_webmail/constants.py
+++ modoboa_webmail/constants.py
@@ -6,5 +6,6 @@ from django.utils.translation import ugettext_lazy as _
 SORT_ORDERS = [
     ("date", _("Date")),
     ("from", _("Sender")),
+    ("size", _("Size")),
     ("subject", _("Subject")),
 ]

65e1ccab4d333a0d21806df5b266f419cc40dfd0
Antoine Nguyen <tonio@ngyn.org>
2022-05-04 10:10:46 +0200

    Updated test config

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -150,6 +150,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 

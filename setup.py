fc62897f9fc17d1a70f87e883d1707c8e6cdf3a4
John Paul <iamjohnpaulr5@gmail.com>
2024-01-25 07:40:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ https://packaging.python.org/en/latest/distributing.html
 
 import io
 from os import path
+from setuptools import setup, find_packages
+
 try:
     from pip.req import parse_requirements
 except ImportError:
     # pip >= 10
     from pip._internal.req import parse_requirements
-from setuptools import setup, find_packages
 
 
 def get_requirements(requirements_file):
@@ -24,28 +25,23 @@ def get_requirements(requirements_file):
     if path.isfile(requirements_file):
         for req in parse_requirements(requirements_file, session="hack"):
             try:
-                # check markers, such as
-                #
-                #     rope_py3k    ; python_version >= '3.0'
-                #
                 if req.match_markers():
                     requirements.append(str(req.req))
             except AttributeError:
-                # pip >= 20.0.2
                 requirements.append(req.requirement)
     return requirements
 
 
 if __name__ == "__main__":
     HERE = path.abspath(path.dirname(__file__))
-    INSTALL_REQUIRES = get_requirements(path.join(HERE, "requirements.txt"))
+    REQUIREMENTS_FILE = path.join(HERE, "requirements.txt")
+    INSTALL_REQUIRES = get_requirements(REQUIREMENTS_FILE)
 
     with io.open(path.join(HERE, "README.rst"), encoding="utf-8") as readme:
         LONG_DESCRIPTION = readme.read()
 
     def local_scheme(version):
-        """Skip the local version (eg. +xyz of 0.6.1.dev4+gdf99fe2)
-            to be able to upload to Test PyPI"""
+        """Skip the local version to be able to upload to Test PyPI."""
         return ""
 
     setup(

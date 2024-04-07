f13293104378de2f03580fcae269b2c5635ef0f5
John Paul <iamjohnpaulr5@gmail.com>
2024-04-07 18:24:32 +0530

    Update manage.py with few changes
    simplifies error handling, and provides a clearer error message when Django import fails

--- test_project/manage.py
+++ test_project/manage.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import unicode_literals
-
 import os
 import sys
 
@@ -9,17 +7,10 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django  # noqa
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
+    except ImportError as e:
+        raise ImportError(
+            "Failed to import Django. Make sure Django is installed and "
+            "available on your PYTHONPATH, or activate a virtual environment."
+        ) from e
+
     execute_from_command_line(sys.argv)
